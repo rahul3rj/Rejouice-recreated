24219387f66e9a41e919412aa50bf2a862913875
rahul3rj <jharahul3rj@gmail.com>
2024-08-30 20:27:07 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,193 @@
+function locoscroll(){
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+
+}
+locoscroll();
+
+function cursorEffect() {
+var page1Content = document.querySelector("#page1-content");
+var cursor = document.querySelector("#cursor");
+
+page1Content.addEventListener("mousemove", function(val){
+    gsap.to(cursor,{
+        x:val.x,
+        y:val.y,
+    })
+})
+page1Content.addEventListener("mouseenter", function(){
+    gsap.to(cursor,{
+        scale:1,
+        opacity:1
+    })
+})
+page1Content.addEventListener("mouseleave", function(){
+    gsap.to(cursor,{
+        scale:0,
+        opacity:0
+    })
+})
+}
+cursorEffect();
+
+function textanime1(){
+    gsap.from(".elemText h1, .elemText h2, .elemText h3", {
+
+    y:120,
+    stagger:0.1,
+    duration:3,
+    ease: "power3.in",
+    scrollTrigger:{
+        trigger:"#page2",
+        scroller:"#main",
+        start:"top 87%",
+        end:"top 86%",
+        // markers:true,
+        scrub:3
+    }
+})
+}
+textanime1();
+
+function lineanime1(){
+    gsap.from("#line", {
+
+    x:-1000,
+    stagger:0.1,
+    duration:3,
+    ease: "power3.in",
+    scrollTrigger:{
+        trigger:"#page2",
+        scroller:"#main",
+        start:"top 87%",
+        end:"top 86%",
+        // markers:true,
+        scrub:3
+    }
+})
+}
+lineanime1();
+
+function textanime3(){
+    gsap.from(".elemText3 h1", {
+
+    y:100,
+    stagger:0.1,
+    duration:1,
+    ease: "power3.in",
+    scrollTrigger:{
+        trigger:"#page3",
+        scroller:"#main",
+        start:"top 87%",
+        end:"top 86%",
+        // markers:true,
+        scrub:2
+    }
+})}
+textanime3();
+
+function textanime4(){
+    gsap.from(".elemText4 h1, .elemText4 h2, .elemText4 h3", {
+
+    y:120,
+    stagger:0.1,
+    duration:3,
+    ease: "power3.in",
+    scrollTrigger:{
+        trigger:"#page4",
+        scroller:"#main",
+        start:"top 87%",
+        end:"top 86%",
+        // markers:true,
+        scrub:3
+    }
+})
+}
+textanime4();
+
+function textanime6(){
+    gsap.from(".elemText6 h1, .elemText6 h2, .elemText6 h3", {
+
+    y:120,
+    stagger:0.1,
+    duration:3,
+    ease: "power3.in",
+    scrollTrigger:{
+        trigger:"#page6",
+        scroller:"#main",
+        start:"top 87%",
+        end:"top 86%",
+        // markers:true,
+        scrub:3
+    }
+})
+}
+textanime6();
+
+function videoplay(){
+const videos = document.querySelectorAll('.hover-video');
+
+videos.forEach(video => {
+    video.addEventListener('mouseenter', () => {
+        video.play();
+    });
+    video.addEventListener('mouseleave', () => {
+        video.pause();
+        video.currentTime = 0; 
+    });
+});
+}
+videoplay();
+
+function cursorEffect5() {
+    var page1Content = document.querySelector("#extra");
+    var cursor = document.querySelector("#p5_cursor");
+    
+    page1Content.addEventListener("mousemove", function(val){
+        gsap.to(cursor,{
+            x:val.x,
+            y:val.y,
+        })
+    })
+    page1Content.addEventListener("mouseenter", function(){
+        gsap.to(cursor,{
+            scale:1,
+            opacity:1
+        })
+    })
+    page1Content.addEventListener("mouseleave", function(){
+        gsap.to(cursor,{
+            scale:0,
+            opacity:0
+        })
+    })
+}
+cursorEffect5();
+
